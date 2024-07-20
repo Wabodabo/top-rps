14de1b0e76ef5e68eb6b90a5ebe1c4c8807b47f0
Lau Poppe <laurens_maple@hotmail.com>
2024-07-20 08:50:48 +0200

    Finished the game

--- rps.js
+++ rps.js
@@ -1,16 +1,58 @@
+
+
 function getComputerChoice(){
     let num = Math.random();
     if(num<1/3){
-        return "Rock";
+        return "rock";
     }else if(num>2/3){
-        return "Scissors";
+        return "scissors";
     }else{
-        return "Paper";
+        return "paper";
     }
 }
 
 function getHumanChoice(){
-    let input = prompt("Play the game! Choose Rock, Paper or Scissors!");
+    let input = prompt("Play the game! Choose Rock, Paper or Scissors!").toLowerCase();
     console.log(input);
     return input;
 }
+
+
+function playGame(){
+    const rounds = 5;
+    let humanScore = 0;
+    let computerScore = 0;
+
+    function playRound(humanChoice, computerChoice){
+
+        if(humanChoice == "paper" && computerChoice =="rock"){
+            humanScore++;
+            console.log("You win! Paper beats Rock");
+        }else if(humanChoice=="rock" && computerChoice=="scissors"){
+            humanScore++;
+            console.log("You win! Rock beats Scissors");
+        }else if(humanChoice=="scissors" && computerChoice=="paper"){
+            humanScore++;
+            console.log("You win! Scissors beats Paper");
+        }else if(humanChoice == computerChoice){
+            console.log("It's a draw!");
+        }else {
+            computerScore++;
+            console.log(`You lose! ${humanChoice} loses from ${computerChoice} mwua mwua mwa :(`)
+        }
+
+    }
+
+    while(humanScore+computerScore<rounds){
+        let humanChoice = getHumanChoice();
+        let computerChoice = getComputerChoice();
+
+        playRound(humanChoice, computerChoice);
+    }
+    if(humanScore>=3){
+        return `You won! You won ${humanScore} out of 5 matches`;
+    }else{
+        return `You lost :( You only won ${humanScore} games out of 5)`;
+    }
+}
+

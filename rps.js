2eef91590ffa91cfdc011704f7b7dce0b941f16e
Lau Poppe <laurens_maple@hotmail.com>
2024-07-31 06:59:26 +0200

    Added UI interface to the game

--- rps.js
+++ rps.js
@@ -12,47 +12,78 @@ function getComputerChoice(){
 }
 
 function getHumanChoice(){
-    let input = prompt("Play the game! Choose Rock, Paper or Scissors!").toLowerCase();
+    
     console.log(input);
     return input;
 }
 
 
-function playGame(){
-    const rounds = 5;
-    let humanScore = 0;
-    let computerScore = 0;
+let buttons = document.querySelector("#buttons");
+let humanScore = 0;
+let computerScore = 0;
 
-    function playRound(humanChoice, computerChoice){
+buttons.addEventListener('click', (event)=>{
+    let target = event.target;
+    let computerChoice = getComputerChoice();
+    let result;
 
-        if(humanChoice == "paper" && computerChoice =="rock"){
-            humanScore++;
-            console.log("You win! Paper beats Rock");
-        }else if(humanChoice=="rock" && computerChoice=="scissors"){
-            humanScore++;
-            console.log("You win! Rock beats Scissors");
-        }else if(humanChoice=="scissors" && computerChoice=="paper"){
-            humanScore++;
-            console.log("You win! Scissors beats Paper");
-        }else if(humanChoice == computerChoice){
-            console.log("It's a draw!");
-        }else {
-            computerScore++;
-            console.log(`You lose! ${humanChoice} loses from ${computerChoice} mwua mwua mwa :(`)
+    switch(target.id){
+        case 'rock':            
+            result = playRound('rock', computerChoice);
+        break;
+        case 'scissors':
+            result = playRound('scissors', computerChoice);
+        break;
+        case 'paper':
+            result = playRound('paper', computerChoice);
+        break;
+    }
+
+    if(result == 1){
+        humanScore++;
+    }else if(result == -1){
+        computerScore++;
+    }
+
+    let outputDiv = document.querySelector("#results");
+
+    if(humanScore == 5 || computerScore == 5){
+        if (humanScore == 5){
+            outputDiv.textContent = "You won! You were the first to reach 5 points.";
+        } else{
+            outputDiv.textContent = "You lost! The computer reached 5 points first.";
         }
-
-    }
-
-    while(humanScore+computerScore<rounds){
-        let humanChoice = getHumanChoice();
-        let computerChoice = getComputerChoice();
-
-        playRound(humanChoice, computerChoice);
-    }
-    if(humanScore>=3){
-        return `You won! You won ${humanScore} out of 5 matches`;
     }else{
-        return `You lost :( You only won ${humanScore} games out of 5)`;
+        outputDiv.textContent = `You: ${humanScore} points, Computer: ${computerScore} points`
     }
+});
+
+
+
+function playRound(humanChoice, computerChoice){
+
+    if(humanChoice == "paper" && computerChoice =="rock"){
+        console.log("You win! Paper beats Rock");
+        return 1;        
+    }else if(humanChoice=="rock" && computerChoice=="scissors"){
+        
+        console.log("You win! Rock beats Scissors");
+        return 1;
+    }else if(humanChoice=="scissors" && computerChoice=="paper"){
+        
+        console.log("You win! Scissors beats Paper");
+        return 1;
+    }else if(humanChoice == computerChoice){
+        
+        console.log("It's a draw!");
+        return 0;
+    }else {
+        
+        console.log(`You lose! ${humanChoice} loses from ${computerChoice} mwua mwua mwa :(`)
+        return -1;
+    }
+
 }
+
+
 
